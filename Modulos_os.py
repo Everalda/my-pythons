5a2f9874bd729dfc7d04a51feb620862fc2bf7ab
Everalda <esms@ecomp.poli.br>
2018-01-06 19:35:11 -0300

    Modulos_os

--- Modulos_os.py
+++ Modulos_os.py
@@ -1,6 +1,12 @@
+'''abrir arquivo com os dados de saida para o usuario em duas formas'''
 import os
-'''atalho =os.startfile
+'''
+1) forma
+atalho =os.startfile
 atalho('C:/Users/EVA/Documents/my-pythons/arquivos/tabela.txt')
 '''
+
+'''2) forma'''
+
 a=os.system
 a('C:/Users/EVA/Documents/my-pythons/arquivos/tabela.txt')
