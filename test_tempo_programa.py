675a2b794906dde3cfd11657c1b6091b124a657a
Everalda <esms@ecomp.poli.br>
2018-01-07 10:45:32 -0300

    tempo pc

---
+++ test_tempo_programa.py
@@ -0,0 +1,8 @@
+from time import time
+tempo = time()
+i=0
+print 'The fox jumped the fence!!, now the dogs are running to get the fox, the fox is smart she runned away'
+while i < 100:
+   i=i+1
+   print i 
+print 'A raposa correu muito rapido exatamente:', time()-tempo, 'segundos'
